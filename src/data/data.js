e7d917ff4b98044329486f0101ec564c8919ae45
Oriana11 <rubyjpy@gmail.com>
2023-06-27 16:02:45 +0200

    Added data to data.js and created assets

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+export const products = [
+    { id: 1, name: "Product 1", price: 100, image: "tshirt1.png" },
+    { id: 2, name: "Product 2", price: 200, image: "tshirt2.png" },
+    { id: 3, name: "Product 3", price: 300, image: "tshirt3.png" },
+    { id: 4, name: "Product 4", price: 400, image: "tshirt4.png" },
+    { id: 5, name: "Product 5", price: 500, image: "tshirt5.png" },
+    { id: 6, name: "Product 6", price: 600, image: "tshirt6.png" },
+];
